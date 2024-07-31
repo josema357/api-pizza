7a0e1db36f989a60bf0c8c3284705f81698adf2a
Jose Manuel <j.ochoa317@gmail.com>
2024-07-31 11:38:42 -0500

    add: authorities

--- src/main/java/com/pizzeria/service/UserSecurityService.java
+++ src/main/java/com/pizzeria/service/UserSecurityService.java
@@ -4,6 +4,12 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 
 import com.pizzeria.persistence.entity.UserEntity;
@@ -25,10 +31,28 @@ public class UserSecurityService implements UserDetailsService {
     return User.builder()
       .username(user.getUsername())
       .password(user.getPassword())
-      .roles(roles)
+      .authorities(this.grantedAuthorities(roles))
       .accountLocked(user.getLocked())
       .disabled(user.getDisabled())
       .build();
+  }
+
+  private String[] getAuthorities(String role){
+    if("ADMIN".equals(role) || "CUSTOMER".equals(role)){
+      return new String[] {"random_order"};
+    }
+    return new String[] {};
+  }
+
+  private List<GrantedAuthority> grantedAuthorities(String[] roles){
+    List<GrantedAuthority> authorities = new ArrayList<>(roles.length);
+    for (String role : roles) {
+      authorities.add(new SimpleGrantedAuthority("ROLE_" + role));
+      for (String authority : this.getAuthorities(role)) {
+        authorities.add(new SimpleGrantedAuthority(authority));
+      }
+    }
+    return authorities;
   }
   
 }
