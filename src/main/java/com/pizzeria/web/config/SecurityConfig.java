f4e4d42ff92a11630639a99e976b816295a0976d
Jose Manuel <j.ochoa317@gmail.com>
2024-08-22 23:44:31 -0500

    add: apply filter in SecurityConfig

--- src/main/java/com/pizzeria/web/config/SecurityConfig.java
+++ src/main/java/com/pizzeria/web/config/SecurityConfig.java
@@ -8,21 +8,29 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
+  private final JwtFilter jwtFilter;
+
+  public SecurityConfig(JwtFilter jwtFilter){
+    this.jwtFilter = jwtFilter;
+  }
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
       http
         .csrf(csrf -> csrf.disable())
         .cors(Customizer.withDefaults())
+        .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests((authorizeHttpRequests) ->
           authorizeHttpRequests
-            .requestMatchers("/api/auth/login").permitAll()
+            .requestMatchers("/api/auth/**").permitAll()
             .requestMatchers("/api/customers/**").hasAnyRole("ADMIN", "CUSTOMER")
             .requestMatchers(HttpMethod.GET, "/api/pizzas/**").hasAnyRole("ADMIN","CUSTOMER")
             .requestMatchers(HttpMethod.POST, "/api/pizzas/**").hasRole("ADMIN")
@@ -31,7 +39,7 @@ public class SecurityConfig {
             .requestMatchers("/api/orders/**").hasRole("ADMIN")
             .anyRequest()
             .authenticated())
-        .httpBasic(Customizer.withDefaults());
+        .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
     return http.build();
   }
 
