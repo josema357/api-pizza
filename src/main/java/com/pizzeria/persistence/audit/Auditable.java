63f15e19d3d864987f1b618457f54761c5b89ae9
Jose Manuel <j.ochoa317@gmail.com>
2024-08-26 23:04:40 -0500

    add: audit users in the DB

--- src/main/java/com/pizzeria/persistence/audit/Auditable.java
+++ src/main/java/com/pizzeria/persistence/audit/Auditable.java
@@ -2,7 +2,9 @@ package com.pizzeria.persistence.audit;
 
 import java.time.LocalDateTime;
 
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import jakarta.persistence.Column;
@@ -16,4 +18,10 @@ public class Auditable {
   @Column(name = "modified_date")
   @LastModifiedDate
   private LocalDateTime modifiedDate;
+  @Column(name = "created_by")
+  @CreatedBy
+  private String createdBy;
+  @Column(name = "modified_by")
+  @LastModifiedBy
+  private String modifiedBy;
 }
